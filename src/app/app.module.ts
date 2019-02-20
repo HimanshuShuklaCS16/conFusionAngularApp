b3e06a7c3d2739d89a448170bce9468c4b634fe4
unknown <himanshushukla16398@gmail.com>
2019-02-21 00:53:37 +0530

    assignment3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
 import {FormsModule} from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -55,6 +56,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     FlexLayoutModule,
     MatListModule,
     MatCardModule,
--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit ,Input,ViewChild} from '@angular/core';
 import {Dish} from '../shared/Dish';
 import {Params,ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {DishService} from '../services/dish.service';
 import {switchMap } from 'rxjs/operators';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {Comment} from '../shared/comment';
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
   styleUrls: ['./dish-detail.component.scss']
 })
 export class DishDetailComponent implements OnInit {
-
+@ViewChild("newform") commentFormDirective;
+commentForm:FormGroup;
+comment:Comment;
 dish:Dish;
 dishIds:string[];
 prev:string;
 next:string;
+
   constructor(private dishservice:DishService,
   private route:ActivatedRoute,
-private location:Location) { }
+private location:Location,private fb:FormBuilder) {
+this.createForm();
+ }
+ formErrors={
+   'author':'',
+   'comment':'',
+   'rating':''
+ };
 
+ validationmessages={
+   'author':{
+     'required':'First name is required',
+     'minlength':'First name should be at least 2 characters long'
+   },
+   'comment':{
+     'required':'Comment is required'
+   }
+ };
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds=> this.dishIds=dishIds);
        const id = this.route.params.pipe(switchMap((params:Params)=> this.dishservice.getDish(params['id'])))
@@ -29,8 +50,49 @@ private location:Location) { }
   this.prev=this.dishIds[(this.dishIds.length+index-1)%this.dishIds.length];
   this.next=this.dishIds[(this.dishIds.length+index+1)%this.dishIds.length];
   }
-
+  createForm():void{
+    this.commentForm=this.fb.group({
+      comment:['',[Validators.required]],
+      author:['',[Validators.required,Validators.minLength(2)]],
+      rating:[5]
+    });
+    this.commentForm.valueChanges
+    .subscribe(data=>this.onValueChanged(data));
+    this.onValueChanged();
+  }
+  onValueChanged(data?: any) {
+     if (!this.commentForm) { return; }
+     const form = this.commentForm;
+     for (const field in this.formErrors) {
+       if (this.formErrors.hasOwnProperty(field)) {
+         // clear previous error message (if any)
+         this.formErrors[field] = '';
+         const control = form.get(field);
+         if (control && control.dirty && !control.valid) {
+           const messages = this.validationmessages[field];
+           for (const key in control.errors) {
+             if (control.errors.hasOwnProperty(key)) {
+               this.formErrors[field] += messages[key] + ' ';
+             }
+           }
+         }
+       }
+     }
+   }
 goBack():void{
   this.location.back();
+}
+onSubmit(){
+  this.comment=this.commentForm.value;
+  var d = new Date();
+var n = d.toISOString();
+  this.comment.date=n;
+  this.dish.comments.push(this.comment);
+  console.log(this.comment);
+  this.commentForm.reset({
+    author:'',
+    comment:'',
+    rating:5
+  });
 }
 }
